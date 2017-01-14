545baa3623c7d6b8f87cabd7182efd7041415209
bertverleyen <bert.verleyen.r6951@student.hogent.be>
2017-01-14 11:53:46 +0100

    2de commit asp.net c# app cv

---
+++ CVBertVerleyen/CVBertVerleyen/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVBertVerleyen.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home pagina CV online";
+           
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About me";
+
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            ViewBag.Title = "Projects";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+
+            return View();
+        }
+    }
+}
